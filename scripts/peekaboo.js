636c0e40f7b288919588643dca3c230fc8c6c0d5
Abdjul <60339265+Abdjulaziz@users.noreply.github.com>
2020-04-13 21:32:50 +0200

    Made peekaboo closeable

--- scripts/peekaboo.js
+++ scripts/peekaboo.js
@@ -1,6 +1,15 @@
 // peekaboo transition på scroll
 function yScrollHandler() {
     var win = document.getElementById("peekaboo");
+    var exit = document.querySelector(".peekExit");
+    
+    exit.addEventListener('click', () => {
+        if(win.style.left){
+            win.style.left = "-852px";
+        } else {
+            win.style.left = "-852px";
+        }
+    });
     if ((window.pageYOffset + window.innerHeight) >= document.body.offsetHeight) {
         //win.style.webkitTransition = 
         win.style.transition = "left 1s ease-in-out 0.1s";
@@ -8,8 +17,6 @@ function yScrollHandler() {
     } else {
         win.style.transition = "left 0.5s ease-in-out 0.2s";
         win.style.left = "-852px";
-    } 
+    }
 }
-
-  
 window.onscroll = yScrollHandler;
