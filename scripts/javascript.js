e857c3d1f58c24db8213bf64c6dc559324ccb118
Mads Hougesen <madshougesen@gmail.com>
2020-08-23 13:42:04 +0200

    cleanup of the code to make it more presentable

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,51 +1,52 @@
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-    const btnScrollToTop = document.querySelector('#btnScrollToTop');
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+  const btnScrollToTop = document.querySelector("#btnScrollToTop");
 
+  burger.addEventListener("click", () => {
+    //Toggle Nav
+    nav.classList.toggle("nav-active");
 
-    burger.addEventListener('click', () => {
-        //Toggle Nav
-        nav.classList.toggle('nav-active');
-
-        //Animate links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards  ${index / 12 + 0.5}s`;
-            }
-        });
-        //Burger Animation
-        burger.classList.toggle('toggle');
+    //Animate links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards  ${
+          index / 12 + 0.5
+        }s`;
+      }
     });
-}
+    //Burger Animation
+    burger.classList.toggle("toggle");
+  });
+};
 
 navSlide();
 
 //Scroll to top knap
 btnScrollToTop.addEventListener("click", function () {
-    window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: "smooth"
-    });
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
 });
 
 // Produktside tabs - niks pille!!
 
 function openProduktInfo(evt, produktInfo) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(produktInfo).style.display = "block";
-    evt.currentTarget.className += " active";
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(produktInfo).style.display = "block";
+  evt.currentTarget.className += " active";
 }
 document.getElementById("defaultOpen").click();
--- scripts/peekaboo.js
+++ scripts/peekaboo.js
@@ -1,22 +1,22 @@
 // peekaboo transition på scroll
 function yScrollHandler() {
-    var win = document.getElementById("peekaboo");
-    var exit = document.querySelector(".peekExit");
-    
-    exit.addEventListener('click', () => {
-        if(win.style.left){
-             win.style.display = "none"
-        } else {
-            win.style.left = "-852px";
-        }
-    });
-    if ((window.pageYOffset + window.innerHeight) >= document.body.offsetHeight) {
-        //win.style.webkitTransition = 
-        win.style.transition = "left 1s ease-in-out 0.1s";
-        win.style.left = "0px";
+  var win = document.getElementById("peekaboo");
+  var exit = document.querySelector(".peekExit");
+
+  exit.addEventListener("click", () => {
+    if (win.style.left) {
+      win.style.display = "none";
     } else {
-        win.style.transition = "left 0.5s ease-in-out 0.2s";
-        win.style.left = "-852px";
+      win.style.left = "-852px";
     }
+  });
+  if (window.pageYOffset + window.innerHeight >= document.body.offsetHeight) {
+    //win.style.webkitTransition =
+    win.style.transition = "left 1s ease-in-out 0.1s";
+    win.style.left = "0px";
+  } else {
+    win.style.transition = "left 0.5s ease-in-out 0.2s";
+    win.style.left = "-852px";
+  }
 }
 window.onscroll = yScrollHandler;
